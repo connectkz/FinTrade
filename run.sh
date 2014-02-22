1e33dcd693db76d1f25361756163225e4b280f50
connectkz <connect@mail.kz>
2014-02-23 02:11:02 +0600

    first file

---
+++ run.sh
@@ -0,0 +1,22 @@
+cp /data/FileStorage/share/FinTr/modules/FinTr_cashbox/Menu.php /var/www/connect/modules/FinTr_cashbox/Menu.php
+
+cp /data/FileStorage/share/FinTr/modules/FinTr_CashOut/logic_hooks.php /var/www/connect/custom/modules/FinTr_CashOut/logic_hooks.php
+cp /data/FileStorage/share/FinTr/modules/FinTr_CashOut/CashOut.php /var/www/connect/custom/modules/FinTr_CashOut/CashOut.php
+
+cp /data/FileStorage/share/FinTr/modules/FinTr_CashIn/logic_hooks.php /var/www/connect/custom/modules/FinTr_CashIn/logic_hooks.php
+cp /data/FileStorage/share/FinTr/modules/FinTr_CashIn/CashIn.php /var/www/connect/custom/modules/FinTr_CashIn/CashIn.php
+cp /data/FileStorage/share/FinTr/modules/FinTr_CashIn/CashIn_MoneyIn.php /var/www/connect/custom/modules/FinTr_CashIn/CashIn_MoneyIn.php
+cp /data/FileStorage/share/FinTr/modules/FinTr_CashIn/Ext/Layoutdefs/layoutdefs.ext.php /var/www/connect/custom/modules/FinTr_CashIn/Ext/Layoutdefs/layoutdefs.ext.php
+
+cp /data/FileStorage/share/FinTr/modules/FinTr_ToWarehouse/logic_hooks.php /var/www/connect/custom/modules/FinTr_ToWarehouse/logic_hooks.php
+cp /data/FileStorage/share/FinTr/modules/FinTr_ToWarehouse/ToWarehouse.php /var/www/connect/custom/modules/FinTr_ToWarehouse/ToWarehouse.php
+
+cp /data/FileStorage/share/FinTr/modules/FinTr_FromWarehouse/logic_hooks.php /var/www/connect/custom/modules/FinTr_FromWarehouse/logic_hooks.php
+cp /data/FileStorage/share/FinTr/modules/FinTr_FromWarehouse/FromWarehouse.php /var/www/connect/custom/modules/FinTr_FromWarehouse/FromWarehouse.php
+
+cp /data/FileStorage/share/FinTr/modules/FinTr_BankIn/logic_hooks.php /var/www/connect/custom/modules/FinTr_BankIn/logic_hooks.php
+cp /data/FileStorage/share/FinTr/modules/FinTr_BankIn/BankIn.php /var/www/connect/custom/modules/FinTr_BankIn/BankIn.php
+
+
+
+
